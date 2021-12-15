e4d4990c253dfb64b89f60ad4dc6716b7c957ee0
olitskaya <94138631+olitskaya@users.noreply.github.com>
2021-12-15 22:31:22 +0200

    Create Task_1.py

---
+++ Task_1.py
@@ -0,0 +1,40 @@
+# Перепишіть програму-банкомат на використання бази даних 
+# для збереження всих даних.
+# Використовувати БД sqlite3 та натівний Python.
+# 
+# Дока з прикладами: https://docs.python.org/3/library/
+# sqlite3.html
+# Туторіал (один із): https://www.sqlitetutorial.net/sqlite-
+# python/
+#
+# Для уніфікації перевірки, в базі повинні бути 3 користувача:
+# ім'я: user1, пароль: user1
+# ім'я: user2, пароль: user2
+# ім'я: admin, пароль: admin (у цього користувача - права інкасатора)
+
+import sqlite3
+
+con = sqlite3.connect('atm.db')
+cur = con.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS users
+               (id integer PRIMARY KEY, name text NOT NULL, password text NOT NULL, is_incassator bit DEFAULT False)''')
+cur.execute("INSERT INTO users VALUES ('1','user1','user1')")
+cur.execute("INSERT INTO users VALUES ('2','user2','user2')")
+cur.execute("INSERT INTO users VALUES ('3','admin','admin', True)")
+con.commit()
+cur.execute('''CREATE TABLE IF NOT EXISTS balance
+               (user_id integer NOT NULL, amount integer NOT NULL DEFAULT 0, FOREIGN KEY (user_id) REFERENCES users(id))''')
+cur.execute("INSERT INTO balance VALUES ('1')")
+cur.execute("INSERT INTO balance VALUES ('2', '100')")
+con.commit()
+cur.execute('''CREATE TABLE IF NOT EXISTS banknotes
+               (id integer, nom integer NOT NULL PRIMARY KEY, quantity integer NOT NULL DEFAULT 0)''')
+cur.execute("INSERT INTO balance VALUES ('1', '10')")
+cur.execute("INSERT INTO balance VALUES ('2', '20')")
+cur.execute("INSERT INTO balance VALUES ('3', '50')")
+cur.execute("INSERT INTO balance VALUES ('4', '100')")
+cur.execute("INSERT INTO balance VALUES ('5', '200')")
+cur.execute("INSERT INTO balance VALUES ('6', '500')")
+cur.execute("INSERT INTO balance VALUES ('7', '1000')")
+
+con.close()
